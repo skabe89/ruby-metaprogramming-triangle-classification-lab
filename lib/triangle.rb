0c95003892748630e431fb8bae7922045daf5e41
Sean <skabe89@gmail.com>
2020-11-25 22:38:00 -0500

    commit tree e36a6fcbe00777bd13dd616760ae5bb113bbd0af

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
+require 'pry'
+
 class Triangle
-  # write code here
+
+  attr_accessor :a, :b, :c
+
+
+  def initialize(a, b, c)
+    @sides = []
+    self.a = a
+      @sides << self.a
+    self.b = b
+      @sides << self.b
+    self.c = c
+      @sides << self.c
+  end
+
+  def kind
+    if @sides.any?{|i| i == 0}
+      raise TriangleError
+    elsif @sides.any?{|i| i < 0}
+      raise TriangleError
+    elsif a + b <= c || b + c <= a || a + c <= b
+      raise TriangleError
+    elsif @sides.uniq.length == 1
+      :equilateral
+    elsif @sides.uniq.length == 2
+      :isosceles
+    elsif @sides.uniq.length == 3
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+    
+  end
+
 end
